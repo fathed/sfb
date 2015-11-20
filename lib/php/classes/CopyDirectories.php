f11c54bc318c1dad20621597bfca928d1f64cdf7
J Gibbens <james.gibbens@gmail.com>
2015-11-20 13:12:40 -0500

    added error handling

---
+++ lib/php/classes/CopyDirectories.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of CopyDirectories
+ *
+ * @author gibbensj
+ */
+class CopyDirectories {
+     public function __construct() {
+        
+    }
+    public function Copy($src,$dst) {
+        $dir = opendir($src);
+        @mkdir($dst);
+        while (false !== ( $file = readdir($dir))) {
+            if (( $file != '.' ) && ( $file != '..' )) {
+                if (is_dir($src . '/' . $file)) {
+                    recurse_copy($src . '/' . $file, $dst . '/' . $file);
+                } else {
+                    copy($src . '/' . $file, $dst . '/' . $file);
+                }
+            }
+        }
+        closedir($dir);
+    }
+}
